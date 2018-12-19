a0c435c18bfb1f1320bce8a5507bccbafc05ec6e
BT_1N3_17@bktech.local <BT_1N3_17@bktech.local>
2018-12-19 11:43:03 -0500

    Initial commit

---
+++ src/com/company/Runner.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import com.sun.net.httpserver.Authenticator;
+
+public class Runner
+{
+    public static void main(String[] args)
+    {
+        int[][] coolArray = {{10, 15, 13, 8}, {12, 6, 9, 7}, {17, 11, 16, 14}};
+        Position partA = successors.findPosition(7, coolArray);
+    }
+}
